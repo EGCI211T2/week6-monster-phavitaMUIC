08e51a3a3bebb8300415bfaf14377d9db9989a24
phavitaMUIC <phavita.cha@student.mahidol.edu>
2026-02-11 04:32:33 +0000

    wewe

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    monster A;
+    monster A("Azurii",20,5);
+    monster B("Zuzu",12,3);
+    monster x[5]={{"Awoo"},{"Raya",10},{"Doi",52,3},{"Lalala",12},{"Cay",10,2}};
+    monster c;
+    
   /*
  
  monster *p,x;
--- monster.h
+++ monster.h
@@ -6,8 +6,25 @@ private:
 	string name;
 	int hp,potion;
 public:
+	void display();
 	void Attack(monster &);
           void heal();
-	monster(string, int,int);
+	monster(string="Anonymous", int=1,int=1);
+	~monster();
+	
 };
+monster::display(){
+	cout<<"Name : "<<name<<endl;
+	cout<<"HP : "<<hp<<endl;
+	cout<<"Potion : "<<potion<<endl;
+}
+monster::monster(string n, int h, int p){
+	name = n;
+	hp = h;
+	potion =p;
+	cout<<"Monster "<<name<<"is here"<<endl; 
+}
+monster::~monster(){
+	cout<<name<< " is gone."<<endl;
+}
 #endif
