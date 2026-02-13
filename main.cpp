571b26ad9512c3a5c4edbb33ffe5f40394bb50a9
phavitaMUIC <phavita.cha@student.mahidol.edu>
2026-02-13 04:18:31 +0000

    wewe

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@ int main(int argc, char* argv[]) {
 
     monster A("Azurii",20,5);
     monster B("Zuzu",12,3);
+    B+=10;
+    A+=B;
+    --A;
+    cout<<"==== The stronger monster is ====\n";
     monster x[5]={{"Awoo"},{"Raya",10},{"Doi",52,3},{"Lalala",12},{"Cay",10,2}};
     monster c;
+    B.display();
     
   /*
  
--- monster.h
+++ monster.h
@@ -7,24 +7,50 @@ private:
 	int hp,potion;
 public:
 	void display();
+	bool operator>(monster &x);
 	void Attack(monster &);
-          void heal();
+    void heal();
 	monster(string="Anonymous", int=1,int=1);
+	void operator+=(int);
+	void operator+=(monster);
+	void operator--(int);
+	monster(int,int);
 	~monster();
 	
 };
+void monster::operator+=(int a){
+	this->hp+=10;
+}
+void monster::operator+=(monster &x){
+	this->hp+=x.hp;
+	x.hp=0;
+}
+void monster::operator--(int a ){
+	hp=-5;
+}
+bool monster::operator>(monster &x){
+	if (hp>x.hp) return true;
+	else return false;
+}
+
 monster::display(){
 	cout<<"Name : "<<name<<endl;
 	cout<<"HP : "<<hp<<endl;
 	cout<<"Potion : "<<potion<<endl;
 }
+
 monster::monster(string n, int h, int p){
 	name = n;
 	hp = h;
 	potion =p;
 	cout<<"Monster "<<name<<"is here"<<endl; 
 }
+
 monster::~monster(){
 	cout<<name<< " is gone."<<endl;
+}
+
+monster::monster(int, int){
+
 }
 #endif
